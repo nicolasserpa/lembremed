5bce67ab0459fb5e2207b5c4ac25f82458404435
nicolasserpa <238610398+nicolasserpa@users.noreply.github.com>
2026-05-23 21:15:59 -0300

    chore: remove private files from repository

--- assets/fonts/font-tools/inspect-font.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const opentype = require('opentype.js');
-const buffer = fs.readFileSync('quicksand-bold.ttf');
-const font = opentype.parse(buffer.buffer.slice(buffer.byteOffset, buffer.byteOffset + buffer.byteLength));
-
-console.log('Units Per Em:', font.unitsPerEm);
-console.log('Names Table:', JSON.stringify(font.names, null, 2));
